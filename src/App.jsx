5c228c58403a786961d989be9c3544280cb1fa28
zGrish <grishmglr0@gmail.com>
2025-07-15 15:53:05 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div className="p-10 bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">Hello Tailwind!</h1>
+      <p className="text-lg text-red-500">Tailwind CSS is working now 🎉</p>
+    </div>
+  );
+}
