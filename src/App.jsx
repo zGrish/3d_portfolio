95dd8ffe4c03f635ca9f7da5c65e631ec2d6ca8d
zGrish <grishmglr0@gmail.com>
2025-07-15 16:40:19 +0530

    Setup Styles, Dummy Content etc.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-export default function App() {
+const App = () => {
   return (
-    <div className="p-10 bg-gray-100">
-      <h1 className="text-4xl font-bold text-blue-600">Hello Tailwind!</h1>
-      <p className="text-lg text-red-500">Tailwind CSS is working now 🎉</p>
+    <div>
+      3D Developer Portfolio
     </div>
-  );
+  )
 }
+
+export default App
