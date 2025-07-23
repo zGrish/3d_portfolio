2a25c1bf4e6e5d5bd8ecf47bae9d30508bdfd4f7
zGrish <grishmglr0@gmail.com>
2025-07-23 21:49:54 +0530

    fix: updated model path of canvas components

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -5,7 +5,7 @@ import { OrbitControls, Preload, useGLTF } from '@react-three/drei'
 import CanvasLoader from '../Loader'
 
 const Earth = () => {
-  const earth = useGLTF('./planet/scene.gltf')
+  const earth = useGLTF('${import.meta.env.BASE_URL}planet/scene.gltf')
 
   return (
     <primitive
